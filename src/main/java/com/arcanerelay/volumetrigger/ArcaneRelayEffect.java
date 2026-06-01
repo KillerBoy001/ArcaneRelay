5b76449d3a5c6f0c88b1a7006e825a9cb09217f4
KillerBoy001 <l.c.zalm@gmail.com>
2026-06-02 00:30:36 +0200

    Code Clean-up
    now using switch-case instead of stacked if's

--- src/main/java/com/arcanerelay/volumetrigger/ArcaneRelayEffect.java
+++ src/main/java/com/arcanerelay/volumetrigger/ArcaneRelayEffect.java
@@ -2,13 +2,12 @@ package com.arcanerelay.volumetrigger;
 
 import com.arcanerelay.ArcaneRelayPlugin;
 
-import com.arcanerelay.util.ArcaneUtil;
-import com.arcanerelay.config.Activation;
-
+import com.arcanerelay.core.activation.ActivationExecutor;
 import com.hypixel.hytale.builtin.triggervolumes.effect.TriggerContext;
 import com.hypixel.hytale.builtin.triggervolumes.effect.TriggerEffect;
 import com.hypixel.hytale.builtin.triggervolumes.manager.VolumeEntry;
 import com.hypixel.hytale.builtin.triggervolumes.shape.TriggerVolumeShape;
+import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.math.block.BlockUtil;
 import com.hypixel.hytale.codec.KeyedCodec;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
@@ -17,12 +16,13 @@ import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.math.util.ChunkUtil;
 import com.hypixel.hytale.math.util.MathUtil;
 
-import com.hypixel.hytale.assetstore.map.DefaultAssetMap;
-import com.hypixel.hytale.server.core.asset.type.item.config.Item;
-
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.chunk.BlockComponentChunk;
 import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
@@ -30,7 +30,7 @@ import com.hypixel.hytale.server.core.util.FillerBlockUtil;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+
 import org.joml.Vector3d;
 import org.joml.Vector3i;
 
@@ -39,17 +39,20 @@ public class ArcaneRelayEffect extends TriggerEffect {
     @Nonnull
     public static final BuilderCodec<ArcaneRelayEffect> CODEC;
     @Nonnull
-    private ArcaneRelayEffect.TriggerType action;
+    private TriggerType action;
+    private Vector3i pos;
+    private PlayerRef PRef;
 
     public ArcaneRelayEffect() {
-        this.action = ArcaneRelayEffect.TriggerType.TRIGGER_ALL;
+        this.action = TriggerType.TRIGGER_ALL;
     }
 
     public void execute(@Nonnull TriggerContext context) {
-        Store<EntityStore> Entstore = context.getStore();
-        World world = ((EntityStore)Entstore.getExternalData()).getWorld();
+        Ref<EntityStore> entityRef = context.getEntityRef();
+        Store<EntityStore> store = context.getStore();
+        World world = ((EntityStore) store.getExternalData()).getWorld();
         if (world != null) {
-            TransformComponent triggerTransform = (TransformComponent) Entstore.getComponent(context.getEntityRef(), TransformComponent.getComponentType());
+            TransformComponent triggerTransform = (TransformComponent) store.getComponent(context.getEntityRef(), TransformComponent.getComponentType());
             Vector3d triggerPos = triggerTransform != null ? triggerTransform.getPosition() : new Vector3d(context.getVolume().getPosition());
             Vector3d min = new Vector3d();
             Vector3d max = new Vector3d();
@@ -78,7 +81,7 @@ public class ArcaneRelayEffect extends TriggerEffect {
                                         if (chunkAtAnchor != null) {
                                             BlockType typeAtAnchor = chunkAtAnchor.getBlockType(anchor.x, anchor.y, anchor.z);
                                             if (typeAtAnchor != null && typeAtAnchor.getId().contains("Pseudo")) {
-                                                this.SendTrigger(world, chunkAtAnchor, typeAtAnchor, anchor.x, anchor.y, anchor.z);
+                                                this.SendTrigger(context,world, chunkAtAnchor, typeAtAnchor, anchor.x, anchor.y, anchor.z);
                                             }
                                         }
                                     }
@@ -102,31 +105,64 @@ public class ArcaneRelayEffect extends TriggerEffect {
         }
     }
 
-    static {
-        CODEC = ((BuilderCodec.Builder)BuilderCodec.builder(
-                ArcaneRelayEffect.class, ArcaneRelayEffect::new, BASE_CODEC)
-                .append(new KeyedCodec("TriggerType", new EnumCodec(ArcaneRelayEffect.TriggerType.class)),
+    static {CODEC = BuilderCodec.builder(
+                    ArcaneRelayEffect.class,
+                    ArcaneRelayEffect::new, BASE_CODEC)
+                .append(new KeyedCodec<>("TriggerType", new EnumCodec<>(TriggerType.class)),
                         (e, v) -> e.action = v, (e) -> e.action)
-                .add())
+                .add()
                 .build();
     }
 
-    public static enum TriggerType {
+    public enum TriggerType {
         TRIGGER_ALL,
         TRIGGER_CONNECTIONS;
     }
 
-    private void SendTrigger(@Nonnull World world, @Nonnull WorldChunk chunk, @Nonnull BlockType blockType, int x, int y, int z) {
-        Vector3i pos = new Vector3i(x, y, z);
-        if (this.action == ArcaneRelayEffect.TriggerType.TRIGGER_ALL){
-            ArcaneRelayPlugin.LOGGER.atInfo().log("VolumeTrigger: WIP Trigger All on block: %s at: %d, %d, %d ", blockType.getId(), x, y, z);
-            // Do Actual Triggering for TRIGGER ALL
-        }
-        if(this.action == ArcaneRelayEffect.TriggerType.TRIGGER_CONNECTIONS){
-            ArcaneRelayPlugin.LOGGER.atInfo().log("VolumeTrigger: WIP Trigger Connections on block: %s at: %d, %d, %d ", blockType.getId(), x, y, z);
-            // Do Actual Triggering for TRIGGER CONNECTIONS
-        }
+    private void SendTrigger(@Nonnull TriggerContext context, @Nonnull World world, @Nonnull WorldChunk chunk, @Nonnull BlockType blockType, int x, int y, int z) {
+        switch(this.action) {
+            //------------------------------//
+            case TRIGGER_ALL:
+                PRef = context.getStore().getComponent(context.getEntityRef(), PlayerRef.getComponentType());
+                PRef.sendMessage(Message.raw("VolumeTrigger: WIP Trigger All on block: "+blockType.getId()+" at: "+x+", "+y+", "+z));
+                ArcaneRelayPlugin.LOGGER.atInfo().log("VolumeTrigger: WIP Trigger All on block: %s at: %d, %d, %d ", blockType.getId(), x, y, z);
 
+
+                // Grabbing some data for usage, just remove what's not needed at the end
+                Ref<ChunkStore> ChunkRef = chunk.getReference();
+                Store<ChunkStore> ChunkStore = ChunkRef.getStore();
+                Vector3i WorldBlockPos = new Vector3i(x, y, z);
+                Vector3i ChunkPos = WorldPosToChunkPos(x, y, z);
+                Vector3i BlockLocalPos = WorldPosToBlockChunkPos(x, y, z);
+                int index = ChunkUtil.indexBlockInColumn(BlockLocalPos.x, BlockLocalPos.y, BlockLocalPos.z);
+                Ref<ChunkStore> blockRef = chunk.getReference();
+
+
+                ActivationExecutor.sendSignals(ChunkStore, blockRef, x, y, z);
+                // Do Actual Triggering for TRIGGER ALL
+                break;
+            //------------------------------//
+            case TRIGGER_CONNECTIONS:
+                PRef = context.getStore().getComponent(context.getEntityRef(), PlayerRef.getComponentType());
+                PRef.sendMessage(Message.raw("VolumeTrigger: WIP Trigger Connections on block: "+blockType.getId()+" at: "+x+", "+y+", "+z));
+                ArcaneRelayPlugin.LOGGER.atInfo().log("VolumeTrigger: WIP Trigger Connections on block: %s at: %d, %d, %d ", blockType.getId(), x, y, z);
+                // Do Actual Triggering for TRIGGER CONNECTIONS
+                break;
+        }
+    }
+
+
+    private Vector3i WorldPosToChunkPos(int x, int y, int z){
+        int cX = x >> 5;
+        int cZ = y >> 5;
+        return new Vector3i(cX,0,cZ);
+    }
+
+    private Vector3i WorldPosToBlockChunkPos(int x, int y, int z){
+        int X = x & 31;
+        int Z = z & 31;
+        int Y = y;
+        return new Vector3i(X,Y,Z);
     }
 
 }
