882c7ce4877ff7ee166cef167b9f453d602489ea
KillerBoy001 <l.c.zalm@gmail.com>
2026-06-05 01:35:53 +0200

    Made Trigger work when entering beam as a player. Added Initial build of Volumetrigger when activating beamer. Still need manual deleting after deleting block

--- src/main/java/com/arcanerelay/config/types/ArcaneBeamerActivation.java
+++ src/main/java/com/arcanerelay/config/types/ArcaneBeamerActivation.java
@@ -1,6 +1,7 @@
 package com.arcanerelay.config.types;
 
 import com.arcanerelay.ArcaneRelayPlugin;
+import com.arcanerelay.components.ArcaneTriggerBlock;
 import com.arcanerelay.config.Activation;
 import com.arcanerelay.components.ArcaneSection;
 import com.arcanerelay.config.Activation;
@@ -11,6 +12,12 @@ import com.arcanerelay.util.ArcaneConnectedBlocksUtil;
 import com.arcanerelay.util.ArcaneUtil;
 import com.arcanerelay.util.BlockVectorUtil;
 import com.hypixel.hytale.assetstore.map.BlockTypeAssetMap;
+import com.hypixel.hytale.builtin.triggervolumes.EntityTargetType;
+import com.hypixel.hytale.builtin.triggervolumes.TriggerVolumesPlugin;
+import com.hypixel.hytale.builtin.triggervolumes.manager.TriggerVolumeManager;
+import com.hypixel.hytale.builtin.triggervolumes.manager.VolumeEntry;
+import com.hypixel.hytale.builtin.triggervolumes.shape.BoxShape;
+import com.hypixel.hytale.builtin.triggervolumes.shape.TriggerVolumeShape;
 import com.hypixel.hytale.codec.Codec;
 import com.hypixel.hytale.codec.KeyedCodec;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
@@ -18,8 +25,10 @@ import com.hypixel.hytale.component.Holder;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.math.util.ChunkUtil;
-import com.hypixel.hytale.math.vector.Vector3d;
-import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.universe.world.chunk.BlockComponentChunk;
+import org.joml.Vector3d;
+import org.joml.Vector3i;
 import com.hypixel.hytale.protocol.BlockMaterial;
 import com.hypixel.hytale.protocol.ChangeVelocityType;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
@@ -35,10 +44,7 @@ import com.hypixel.hytale.server.core.util.TargetUtil;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.*;
 
 
@@ -106,10 +112,12 @@ public class ArcaneBeamerActivation extends Activation {
             if (state.contains("On")) {
                 ArcaneRelayPlugin.LOGGER.atInfo().log("Beamer: Enabling");
                 BuildLaserBeam(commandBuffer, TriggerPos, Triggerchunk, maxRange);
+                CreateTriggerVolume(commandBuffer, TriggerPos, Triggerchunk, maxRange);
                 return ArcaneSection.BlockTickStrategy.PROCESSED;
             } else if (state.contains("Off")) {
                 ArcaneRelayPlugin.LOGGER.atInfo().log("Beamer: Disabling");
                 BlockVectorUtil.setTickingAround(Triggerchunk, TriggerPos, 1);
+                RemoveTriggerVolume(commandBuffer, TriggerPos, Triggerchunk);
                 return ArcaneSection.BlockTickStrategy.PROCESSED;
             }
         }
@@ -123,6 +131,7 @@ public class ArcaneBeamerActivation extends Activation {
             WorldChunk beamerChunk,
             int maxRange
     ) {
+
         BlockTypeAssetMap<String, BlockType> assetMap = BlockType.getAssetMap();
 
         for (int i = 0; i <= maxRange; i++) {
@@ -151,16 +160,122 @@ public class ArcaneBeamerActivation extends Activation {
         }
         return ArcaneSection.BlockTickStrategy.CONTINUE;
     }
+    private void CreateTriggerVolume(
+            ChunkStoreCommandBufferLike commandBuffer,
+            Vector3i BeamerPos,
+            WorldChunk beamerChunk,
+            int maxRange
+    ) {
+        int blockIndex = ChunkUtil.indexBlockInColumn(BeamerPos.x, BeamerPos.y, BeamerPos.z);
+        String name = "Auto_TV_"+blockIndex;
+
+        int Brange = GetBuildRange(commandBuffer,BeamerPos,beamerChunk,maxRange);
+        World world = commandBuffer.getExternalData().getWorld();
+        EntityStore entityStore = world.getEntityStore();
+        Store<EntityStore> store = entityStore.getStore();
+
+        TriggerVolumesPlugin plugin = TriggerVolumesPlugin.get();
+        TriggerVolumeManager manager = store.getResource(plugin.getManagerResourceType());
+        VolumeEntry entry = manager.getVolume(name);
+        if (entry == null) { // Create new TV
+            Vector3i forward = BlockVectorUtil.getForwardVector(beamerChunk,BeamerPos,Brange+1);
+            String worldName = world.getName().toLowerCase(Locale.ROOT);
+            Vector3d Pos = new Vector3d(BeamerPos);
+            Vector3d Start = new Vector3d(0,0,0);
+            Vector3d End = new Vector3d(forward);
+            double Shrink = 0.3F;
+
+            for (int i = 0; i < 3; i++) {
+                if (End.get(i)==0){End.setComponent(i,1);}
+            }
+            for (int i = 0; i < 3; i++) {
+                Start.setComponent(i,Start.get(i)+Shrink);
+                End.setComponent(i,End.get(i)-Shrink);
+            }
+
+            if (End.x <0.1 ){Start.x=1; End.x = End.x+1;} // not sure why but only in this orientations
+
+            TriggerVolumeShape shape = new BoxShape(Start,End);
+
+            entry = new VolumeEntry(name, worldName, Pos, shape, new ArrayList(), EnumSet.of(EntityTargetType.PLAYER), true);
+            manager.register(name, entry);
+            manager.notifyViewersAdd(entry);
+        }else {
+            entry.setEnabled(true);
+            manager.markSpatialDirty();
+            manager.notifyViewersAdd(entry);
+        }
+    }
+
+    private void RemoveTriggerVolume(
+            ChunkStoreCommandBufferLike commandBuffer,
+            Vector3i BeamerPos,
+            WorldChunk beamerChunk
+    ){
+        int blockIndex = ChunkUtil.indexBlockInColumn(BeamerPos.x, BeamerPos.y, BeamerPos.z);
+        String name = "Auto_TV_"+blockIndex;
+
+        World world = commandBuffer.getExternalData().getWorld();
+        EntityStore entityStore = world.getEntityStore();
+        Store<EntityStore> store = entityStore.getStore();
+
+        TriggerVolumesPlugin plugin = TriggerVolumesPlugin.get();
+        TriggerVolumeManager manager = store.getResource(plugin.getManagerResourceType());
+        if (manager != null) {
+            VolumeEntry entry = manager.getVolume(name);
+            if (entry == null) {
+            } else {
+                manager.unregister(name);
+                manager.notifyViewersRemove(name);
+                manager.notifyViewersAdd(entry);
+            }
+        }
+    }
+
+    private int GetBuildRange(
+            ChunkStoreCommandBufferLike commandBuffer,
+            Vector3i BeamerPos,
+            WorldChunk beamerChunk,
+            int maxRange
+    ){
+        int ret =1;
+        for (int i = 0; i <= maxRange; i++) {
+            Vector3i Localforward = BlockVectorUtil.getForwardVector(beamerChunk,BeamerPos,i+1 );
+            Vector3i NextPos = new Vector3i (BeamerPos.x + Localforward.x, BeamerPos.y + Localforward.y, BeamerPos.z + Localforward.z);
+
+            WorldChunk chnk = commandBuffer.getExternalData().getWorld().getChunkIfInMemory(ChunkUtil.indexChunkFromBlock(NextPos.x, NextPos.z));
+            BlockType Block = chnk.getBlockType(NextPos.x,NextPos.y,NextPos.z);
+            if(!BlockVectorUtil.isEmpty(Block)){
+                ret = i;
+                return ret;
+            }
+        }
+        return maxRange;
+    }
 
     private void SendTriggerFromSourceBeamer (
             ChunkStoreCommandBufferLike commandBuffer,
             Vector3i BeamerPos,
             WorldChunk beamerChunk
     ) {
+        World world = commandBuffer.getExternalData().getWorld();
+        Store<ChunkStore> chunkStore = world.getChunkStore().getStore();
         ArcaneRelayPlugin.LOGGER.atInfo().log("Beamer: Send signal now - WIP");
+
+        Ref<ChunkStore> ChunkRef = beamerChunk.getReference();
+        int blockIndex = ChunkUtil.indexBlockInColumn(BeamerPos.x,BeamerPos.y,BeamerPos.z);
+        BlockComponentChunk blockComponentChunk = chunkStore.getComponent(ChunkRef, BlockComponentChunk.getComponentType());
+        Ref<ChunkStore> blockRef = blockComponentChunk.getEntityReference(blockIndex);
+        ArcaneTriggerBlock trigger = chunkStore.getComponent(blockRef, ArcaneRelayPlugin.get().getArcaneTriggerBlockComponentType());
+
+        if (trigger == null) return;
+
+        for (Vector3i out : trigger.getOutputPositions()) {
+            ArcaneUtil.setTicking(chunkStore, out.x, out.y, out.z, BeamerPos.x,BeamerPos.y,BeamerPos.z);
+        }
     }
 
-    Vector3i GetBeamerPosFromLaser(ChunkStoreCommandBufferLike commandBuffer,Vector3i TriggerPos,WorldChunk Chunk,int maxRange){
+    private Vector3i GetBeamerPosFromLaser(ChunkStoreCommandBufferLike commandBuffer,Vector3i TriggerPos,WorldChunk Chunk,int maxRange){
         for (int i = 1; i <= maxRange+1; i++){
             Vector3i LocalBackward = BlockVectorUtil.getForwardVector(Chunk,TriggerPos,i-i*2 );
             Vector3i NextPos = new Vector3i (TriggerPos.x + LocalBackward.x, TriggerPos.y + LocalBackward.y, TriggerPos.z + LocalBackward.z);
